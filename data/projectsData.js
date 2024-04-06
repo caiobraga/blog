7a5892da8fb8e586dd601494d6f5e6b60ee542fb
Caio Gomes Braga <caiotagb@gmail.com>
2024-04-06 14:27:07 -0300

    new changes to projetc data

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,9 +1,19 @@
 const projectsData = [
   {
+    title: 'Código com Café',
+    description: `My projects are centered around understanding and achieving my clients' objectives, ranging from enhancing digital presence and user engagement to streamlining operations and improving resource management. Each project is tailored to not only meet but exceed these goals, ensuring clients receive the most effective and efficient solutions.`,
+    imgSrc: '',
+    href: 'https://www.codigocomcafe.dev.br/',
+    github: 'https://www.codigocomcafe.dev.br/',
+    tech1: 'React.js',
+    tech2: 'Node.js',
+    tech3: 'taiwind css',
+  },
+  {
     title: 'Demo view 3D',
     description: `3D Visualizer and editor`,
     imgSrc: '',
-    href: 'https://www.youtube.com/watch?v=eXIHdj6sIQo&t=1s ',
+    href: 'https://gb-projeto-3d.vercel.app/',
     github: 'https://www.youtube.com/watch?v=eXIHdj6sIQo&t=1s ',
     tech1: 'Next.js',
     tech2: 'three js',
@@ -18,6 +28,39 @@ const projectsData = [
     tech1: 'Flutter',
     tech2: 'Node.js',
     tech3: 'Javascript',
+  },
+  {
+    title: 'CPET app',
+    description: `Basic app for your pet`,
+    imgSrc: '',
+    href: 'https://youtube.com/shorts/TNZrwwSwJGE',
+    github: 'https://youtube.com/shorts/TNZrwwSwJGE',
+    tech1: 'Flutter',
+    tech2: 'Node.js',
+    tech3: 'Javascript',
+  },
+  {
+    title: 'indooh app',
+    description: `Indooh Media Car is a Startup whose purpose is to transform the way of communicating with the consumer public.
+
+    Managing an advertising system with a tablet installed inside cars via application, making it possible for you to advertise simply and quickly
+    and dynamics`,
+    imgSrc: '',
+    href: 'https://indooh.com.br/',
+    github: 'https://indooh.com.br/',
+    tech1: 'next.js',
+    tech2: 'Node.js',
+    tech3: 'flutter',
+  },
+  {
+    title: 'Quebra galho',
+    description: `In response to the growing demand for freelance and independent professional services, this project was initiated to develop an application that bridges the gap between service providers and those in need of their services. The primary goal was to create a seamless, efficient platform that facilitates connections, streamlines communication, and enables transactions, thereby benefiting both professionals and clients.`,
+    imgSrc: '',
+    href: 'https://play.google.com/store/apps/details?id=com.quebragalho.geandersonlemonte',
+    github: 'https://play.google.com/store/apps/details?id=com.quebragalho.geandersonlemonte',
+    tech1: 'next.js',
+    tech2: 'Node.js',
+    tech3: 'flutter',
   },
 ]
 
